66c77834892c5a963558e753982215f164708ef1
Andy Preston <edgeeffect@gmail.com>
2020-12-28 16:20:57 +0000

    LED pin naming matches button

--- src/led.rs
+++ src/led.rs
@@ -3,15 +3,15 @@ use stm32f4xx_hal::gpio::{gpioc::PC13, Output, PushPull};
 use embedded_hal::digital::v2::{OutputPin, ToggleableOutputPin};
 
 pub struct Led {
-    pc13: PC13<Output<PushPull>>,
+    pin: PC13<Output<PushPull>>,
 }
 
 impl Led {
     pub fn new<M>(
-        pin: PC13<M>
+        pc13: PC13<M>
     ) -> Self {
-        let pc13 = pin.into_push_pull_output();
-        Self { pc13 }
+        let pin = pc13.into_push_pull_output();
+        Self { pin }
     }
 
     pub fn set(
@@ -19,15 +19,15 @@ impl Led {
         enable: bool
     ) {
         if enable {
-            self.pc13.set_high().ok();
+            self.pin.set_high().ok();
         } else {
-            self.pc13.set_low().ok();
+            self.pin.set_low().ok();
         }
     }
 
     pub fn toggle(
         &mut self
     ) {
-        self.pc13.toggle().ok();
+        self.pin.toggle().ok();
     }
 }
